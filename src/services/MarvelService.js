e334874afb7c7c664a27f7ef241822595d588a43
Radik Abubjazov <abubjazov@mail.ru>
2021-11-05 19:23:04 +0300

    randomChar: +updateChar, MarvelService: +getCharacter

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -11,12 +11,20 @@ class MarvelService {
         return await res.json()
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=400&${this._apiKey}`)
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`)
+
+        return this._transformCharacter(res.data.results[0])
     }
 
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`)
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
 
