e6e6107e970907d900864f6ea0c15b63e591703c
Radik Abubjazov <abubjazov@mail.ru>
2021-11-06 16:43:31 +0300

    charList: bug fix

--- src/components/charList/charList.js
+++ src/components/charList/charList.js
@@ -51,12 +51,15 @@ export class  CharList  extends Component {
 const View = ({characters}) => {
     const content = characters.map(character => {
         const {id, name, thumbnail} = character
+        let imgStyle = {'objectFit' : 'cover'}
+
+        if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {imgStyle = {'objectFit' : 'unset'}}
         
         return (
             <li 
             key={id}
             className="char__item">
-                <img src={thumbnail} alt={name}/>
+                <img src={thumbnail} alt={name} style={imgStyle}/>
                 <div className="char__name">{name}</div>
             </li>
         )
